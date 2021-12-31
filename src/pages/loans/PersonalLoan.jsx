c12f5f60efa17324a07ab59b85052ebfa91659e2
Fuad Jamilov <fdjmlv666@gmail.com>
2021-12-31 14:24:11 +0400

    PersonalLoan and CarLoan components are updated

--- src/pages/loans/PersonalLoan.jsx
+++ src/pages/loans/PersonalLoan.jsx
@@ -36,7 +36,7 @@ export default function PersonalLoan() {
                 disabled
               />
             </div>
-            <button type="submit" className="loan btn btn-primary">
+            <button type="submit" className="btn loan btn-primary">
               Calculate
             </button>
           </form>
@@ -73,7 +73,7 @@ export default function PersonalLoan() {
                 disabled
               />
             </div>
-            <button type="submit" className="loan btn btn-primary">
+            <button type="submit" className="btn loan btn-primary">
               Send Request
             </button>
           </form>
