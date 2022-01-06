c3c6f112ee716e1486fd11aa4dc5a0ae16151c84
Fuad Jamilov <fdjmlv666@gmail.com>
2022-01-06 06:00:32 +0400

    TransactionHistory component is updated and pagination is added

--- src/api/transactionService.js
+++ src/api/transactionService.js
@@ -1,14 +1,35 @@
 import axios from "axios";
 import secureLs from "../common/helper";
 
-export default class TransactionService{
+export default class TransactionService {
+  transfer(url, body) {
+    let config = {
+      headers: {
+        Authorization: secureLs.get("Authorization"),
+      },
+    };
+    return axios.post("/api/transactions/" + url, body, config);
+  }
 
-    transfer(url,body){
-        let config = {
-            headers : {
-                Authorization : secureLs.get("Authorization")
-            }
-        }
-        return axios.post("/api/transactions/" + url, body, config);
-    }
+  getAll(page) {
+    let config = {
+      headers: {
+        Authorization: secureLs.get("Authorization"),
+      },
+      params: {
+        page: page,
+      },
+    };
+
+    return axios.get("/api/transactions/findAll", config);
+  }
+
+  getPageCount() {
+    let config = {
+      headers: {
+        Authorization: secureLs.get("Authorization"),
+      },
+    };
+    return axios.get("/api/transactions/countPages", config);
+  }
 }
--- src/pages/transactions/TransactionHistory.jsx
+++ src/pages/transactions/TransactionHistory.jsx
@@ -1,11 +1,79 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./TransactionHistory.css";
 import "react-datepicker/dist/react-datepicker.css";
 import DatePicker from "react-datepicker";
+import { Link } from "react-router-dom";
+import TransactionService from "../../api/transactionService";
+import { formattedBalance } from "../../common/helper";
 
 export default function TransactionHistory() {
   const [fromDate, setFromDate] = useState(new Date());
   const [toDate, setToDate] = useState(new Date());
+  const [transactions, settransactions] = useState([]);
+  const [pageCount, setpageCount] = useState(1);
+  const [realPageCount, setrealPageCount] = useState(1);
+  const [pageGroup, setpageGroup] = useState(0);
+
+  useEffect(() => {
+    let transactionService = new TransactionService();
+
+    transactionService
+      .getPageCount()
+      .then((response) => {
+        setrealPageCount(response.data);
+        if (response.data > 16) {
+          setpageCount(16);
+        } 
+        else {
+          setpageCount(response.data);
+        }
+      })
+      .catch((error) => {});
+
+    transactionService
+      .getAll(1)
+      .then((response) => {
+        settransactions(response.data);
+      })
+      .catch((error) => {});
+  }, []);
+
+  const pageClickHandler = (page) => {
+    let transactionService = new TransactionService();
+
+    transactionService
+      .getAll(page)
+      .then((response) => {
+        settransactions(response.data);
+      })
+      .catch((error) => {});
+  };
+
+  const nextClickHandler = () => {
+    if (realPageCount - 16 < 0) {
+      console.log("No Next Page");
+    } 
+    else if (realPageCount - 16 > 16) {
+      setpageGroup(pageGroup + 1);
+      setrealPageCount(realPageCount - 16);
+      setpageCount(16);
+    }
+     else {
+      setpageGroup(pageGroup + 1);
+      setrealPageCount(realPageCount - 16);
+      setpageCount(realPageCount - 16);
+    }
+  };
+
+  const previousClickHandler = () => {
+    if (pageGroup - 1 < 0) {
+      console.log("No Previous Page");
+    } 
+    else {
+      setpageCount(16);
+      setpageGroup(pageGroup - 1);
+    }
+  };
 
   return (
     <div className="transaction container">
@@ -30,7 +98,7 @@ export default function TransactionHistory() {
             <div className="transaction form-group">
               <label htmlFor="datepicker-to">To</label>
               <div className="input-group date" id="datepicker-to">
-              <DatePicker
+                <DatePicker
                   className="form-control"
                   selected={toDate}
                   onChange={(date) => setToDate(date)}
@@ -82,80 +150,52 @@ export default function TransactionHistory() {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Green Bank</td>
-                <td>$100</td>
-                <td>Transfer</td>
-                <td>15-10-2021</td>
-                <td>13:45</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Internal</td>
-                <td>$150</td>
-                <td>Transfer</td>
-                <td>02-10-2021</td>
-                <td>10:05</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Wolt</td>
-                <td>$15</td>
-                <td>Payment</td>
-                <td>25-09-2021</td>
-                <td>15:21</td>
-              </tr>
-              <tr>
-                <th scope="row">4</th>
-                <td>Green Bank</td>
-                <td>$100</td>
-                <td>Transfer</td>
-                <td>15-10-2021</td>
-                <td>13:45</td>
-              </tr>
-              <tr>
-                <th scope="row">5</th>
-                <td>Internal</td>
-                <td>$150</td>
-                <td>Transfer</td>
-                <td>02-10-2021</td>
-                <td>10:05</td>
-              </tr>
-              <tr>
-                <th scope="row">6</th>
-                <td>Wolt</td>
-                <td>$15</td>
-                <td>Payment</td>
-                <td>25-09-2021</td>
-                <td>15:21</td>
-              </tr>
-              <tr>
-                <th scope="row">7</th>
-                <td>Green Bank</td>
-                <td>$100</td>
-                <td>Transfer</td>
-                <td>15-10-2021</td>
-                <td>13:45</td>
-              </tr>
-              <tr>
-                <th scope="row">8</th>
-                <td>Internal</td>
-                <td>$150</td>
-                <td>Transfer</td>
-                <td>02-10-2021</td>
-                <td>10:05</td>
-              </tr>
-              <tr>
-                <th scope="row">9</th>
-                <td>Wolt</td>
-                <td>$15</td>
-                <td>Payment</td>
-                <td>25-09-2021</td>
-                <td>15:21</td>
-              </tr>
+              {transactions.map((transaction, index) => {
+                return (
+                  <tr key={index + 1}>
+                    <th scope="row">
+                      {index + 1 + transaction.limit * transaction.page}
+                    </th>
+                    <td>{transaction.description}</td>
+                    <td>{formattedBalance(transaction.amount)} $</td>
+                    <td>{transaction.type}</td>
+                    <td>{transaction.createdAtDate}</td>
+                    <td>{transaction.createdAtTime}</td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
+          <nav className="pagination-nav" aria-label="Page navigation">
+            <ul className="pagination">
+              <Link className="page-link" to="#">
+                <li onClick={previousClickHandler} className="page-item">
+                  Previous
+                </li>
+              </Link>
+              {[...Array(pageCount)].map((x, i) => {
+                return (
+                  <Link
+                    key={i + 1 + pageGroup * 16}
+                    className="page-link"
+                    to="#"
+                  >
+                    <li
+                      onClick={() => pageClickHandler(i + 1 + pageGroup * 16)}
+                      className="page-item"
+                    >
+                      {i + 1 + pageGroup * 16}
+                    </li>
+                  </Link>
+                );
+              })}
+              <Link className="page-link" to="#">
+                <li onClick={nextClickHandler} className="page-item">
+                  Next
+                </li>
+              </Link>
+            </ul>
+          </nav>
         </div>
       </div>
     </div>
